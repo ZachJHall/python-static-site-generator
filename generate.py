2df3aa54de29c4b6e7309c202c6dda4b754b3617
zachjhall <zachjohnhall@gmail.com>
2021-05-25 02:05:51 -0500

    Add genPosts, only last three articles are added on the index

--- generate.py
+++ generate.py
@@ -4,6 +4,7 @@ import markdown
 
 postDir = "posts/"
 output = "public/"
+posts = os.listdir("markdown")
 
 def h1(content):
     return "<h1>" + content + "</h1>"
@@ -21,27 +22,35 @@ def blogPost(name, content, postDir):
 			else:
 				Post.write(line)
 
+def genPosts():
+	for post in posts:
+		name = post[0]
+
+		with open('./templates/post.html', 'r') as postTemplate, open(output+postDir +name + ".html", "w") as postOutput:
+		
+			for line in postTemplate:
+				if(line.strip('\n') == "[content]"):
+					postOutput.write("<h1>" + name  + "</h1>")
+					#Post.write(markdown.markdown(content))
+				else:
+					postOutput.write(line)
 def genIndex():
     with open('./templates/index.html', 'r') as indexTemplate, open(output + "index.html", 'w') as index:
         for line in indexTemplate:
             if(line.strip('\n') == "[content]"):
-                for filename in os.listdir("markdown"):
-                    index.write('\n')
-                    temp = open("markdown/" + filename)
-                    blogPost(filename[:-4], temp.read(), output + postDir)
-                    index.write( a(postDir + filename[:-4]+".html", filename[:-4]) + "\b")
-                    temp.close()
+                index.write('\n')
+                for file in posts[-3:]:
+                    index.write( a(postDir + file[0] + ".html", file[0]) + "\b")
             else:
                 index.write(line)
-
+    
 def main():
- 
     if not os.path.exists("public"):
         os.mkdir("public")
     if not os.path.exists("public/posts"):
         os.mkdir("public/posts")
-
+    genPosts()
     genIndex()
 
-
 main()
+print(posts)
